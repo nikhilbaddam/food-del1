a25c65335df78b9a9a2a836100c692f4b8935df3
nikhil <91113985+nikhilbaddam@users.noreply.github.com>
2024-07-20 13:08:20 +0530

    Update orderController.js
    url frontend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import userModel from "../models/userModel.js";
 //const stripe= new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order for frontend
 const  placeOrder=async(req,res)=>{
-    const frontend_url="http:localhost:5174";
+    const frontend_url="https://food-del1-frontend.onrender.com/";
 
     try {
         const newOrder=new orderModel({
