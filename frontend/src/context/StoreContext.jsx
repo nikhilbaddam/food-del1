4bcf48397d6e0774ba5ef693430a072778ecbfc6
nikhil <91113985+nikhilbaddam@users.noreply.github.com>
2024-07-20 12:53:56 +0530

    Update StoreContext.jsx
    url change

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url="http://localhost:4000";
+  const url="https://food-del1-backend.onrender.com";
   const [token,setToken]=useState("");
   const[food_list,setFoodList]=useState([]);
 
